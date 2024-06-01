c5ee9b8485fe9289321ced7b261854efc4e049ad
alfajoruwu <mcamilla20@alumnos.utalca.cl>
2024-06-01 12:56:54 -0400

    funcionando backend

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 const express = require("express");
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) =>{
-  res.send("Hola mi server en Express");
+// ------------- Conexion a la base de datos -------------
+const pool = mysql.createPool({
+  host: 'db',
+  user: 'root',
+  password: 'example',
+  database: 'ejemplo',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-app.get('/product/:id', (req, res)=>{
-  res.json({
-    id:req.params.id,
-    producto: "producto "+req.params.id,
-    precio:1
+// ------------- Inicial -------------
+app.get("/", (req, res) => {
+  res.send(`
+  <h1>hola, este es el backend de inventario facilito</h1>
+  <h2>estas son las rutas disponibles:</h2>
+  <h3>/Ejemplo_select: </h3>
+  `);
+});
+
+// ------------- Rutas de ejemplo -------------
+app.get('/ejemplo_select', (req, res) => {
+  pool.query('SELECT * FROM uwu', (error, results) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error.stack);
+      res.status(500).send('Error al obtener datos');
+      return;
+    }
+    res.json(results);
   });
 });
 
 
-app.listen(port, () =>{
-  console.log("Puerto: " + port);
+
+
+// ------------- Print del puerto -------------
+app.listen(port, () => {
+  console.log("Servidor escuchando en el puerto: " + port);
 });
