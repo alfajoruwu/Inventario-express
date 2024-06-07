1a0103aeaca18a1eb3285faede37291f04d3f26f
alfajoruwu <mcamilla20@alumnos.utalca.cl>
2024-06-07 00:44:55 -0400

    nuevas rutas

--- main.js
+++ main.js
@@ -69,6 +69,60 @@ app.get("/", (req, res) => {
     </div>
   </div>
   <br/>
+
+  <!-- Vista bodegas -->
+
+  <input type="checkbox" id="collapse-toggle5">
+  <label for="collapse-toggle5" class="collapse-btn boton"> Vista bodegas</label>
+  <div id="collapse-content5" class="collapse-content">
+
+    <!-- Metodos de la vista -->
+
+    <input type="checkbox" id="collapse-toggle6">
+    <label for="collapse-toggle6" class="collapse-btn botonget">/obtener_bodegas {metodo: get}</label>
+    <div id="collapse-content6" class="collapse-content">
+
+      <h3>Ruta</h3>
+      <p>/obtener_bodegas</p>
+      <p>Metodo: GET</p>
+      <h3>Descripcion</h3>
+      <p>Ruta para obtener las bodegas de un usuario</p>
+      <h3>Parametros</h3>
+      <p>{ Usuario_ID: "Number"}</p>
+      <h3>Respuesta</h3>
+      <p> {error 500}(error al obtener datos): Error al obtener datos</p>
+      <p> {respuesta ok}:{"ID": "Number","Nombre": "String"} </p>
+    </div>
+  </div>
+  <br/>
+
+  <!-- Vista Crear Bodegas -->
+
+  <input type="checkbox" id="collapse-toggle7">
+  <label for="collapse-toggle7" class="collapse-btn boton"> Vista Crear Bodegas</label>
+
+  <div id="collapse-content7" class="collapse-content">
+
+    <!-- Metodos de la vista -->
+
+    <input type="checkbox" id="collapse-toggle8">
+    <label for="collapse-toggle8" class="collapse-btn botonpost">/crear_bodega {metodo: post}</label>
+
+    <div id="collapse-content8" class="collapse-content">
+
+      <h3>Ruta</h3>
+      <p>/crear_bodega</p>
+      <p>Metodo: POST</p>
+      <h3>Descripcion</h3>
+      <p>Ruta para crear una bodega</p>
+      <h3>Parametros</h3>
+      <p>{ Nombre: "String", Usuario_ID: "Number"}</p>
+      <h3>Respuesta</h3>
+      <p> {error 500}(error al crear bodega): Error al crear bodega</p>
+      <p> {respuesta ok}: Bodega creada exitosamente </p>
+    </div>
+  </div>
+  <br/>
   
   <style>
     .boton {
@@ -165,11 +219,35 @@ app.post('/crear_usuario', (req, res) => {
   });
 });
 
-//    --------- Vista inventario ------------
+//    --------- Vista bodegas ------------
 
+app.get('/obtener_bodegas', (req, res) => {
+  const { Usuario_ID } = req.body;
+  const sql = 'SELECT B.*  FROM Bodega B JOIN Administra A ON B.ID = A.Bodega_ID WHERE A.Usuario_ID = ?; ';
+  pool.query(sql,[Usuario_ID], (error, results) => {
+    if (error) {
+      console.error('Error al ejecutar la consulta:', error.stack);
+      res.status(500).send('Error al obtener datos');
+      return;
+    }
+    res.json(results);
+  });
+  });
 
+  // -------------- Vista Crear Bodegas --------------
 
-
+  app.post('/crear_bodega', (req, res) => {
+    const { Nombre, Usuario_ID } = req.body;
+    const sql = 'INSERT INTO Bodega (Nombre) VALUES (?);';
+    pool.query(sql, [Nombre], (error, results) => {
+      if (error) {
+        console.error('Error al ejecutar la consulta:', error.stack);
+        res.status(500).send('Error al crear bodega');
+        return;
+      }
+      res.status(201).send('Bodega creada exitosamente');
+    });
+  });
 
 
 
