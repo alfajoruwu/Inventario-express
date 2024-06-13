711637dc27e4d80da7835416b021a93d3ab554ba
alfajoruwu <mcamilla20@alumnos.utalca.cl>
2024-06-12 22:17:16 -0400

    inicio de git

--- init.sql
+++ init.sql
@@ -1,94 +1,16 @@
 -- Create the database
-CREATE DATABASE inventario;
+CREATE DATABASE Ejemplo;
 
 -- Use the created database
-USE inventario;
+USE Ejemplo;
 
 -- Create the user and grant privileges
-CREATE USER 'alfajor'@'%' IDENTIFIED BY 'alfajor';
-GRANT ALL PRIVILEGES ON *.* TO 'alfajor'@'%' WITH GRANT OPTION;
+CREATE USER 'Backend'@'%' IDENTIFIED BY 'Backend';
+GRANT ALL PRIVILEGES ON *.* TO 'Backend'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 -- Create the tables
-CREATE TABLE `Bodega` (
-  `ID` INT AUTO_INCREMENT PRIMARY KEY,
-  `Nombre` VARCHAR(255),
-  `Codigo_Invitacion` VARCHAR(255)
-);
-
-CREATE TABLE `Producto` (
-  `ID` INT AUTO_INCREMENT PRIMARY KEY,
-  `Nombre` VARCHAR(255),
-  `Descripcion` TEXT,
-  `Imagen` VARCHAR(255),
-  `Precio` DECIMAL(10, 2),
-  `Codigo` VARCHAR(255)
-);
-
-CREATE TABLE `Lote` (
-  `ID` INT AUTO_INCREMENT PRIMARY KEY,
-  `Cantidad` INT,
-  `Fecha_Vencimiento` DATE,
-  `Fecha_de_llegada` DATE,
-  `Producto_ID` INT,
-  FOREIGN KEY (`Producto_ID`) REFERENCES `Producto` (`ID`)
-);
-
-CREATE TABLE `Cliente` (
+CREATE TABLE `Ejemplo` (
   `ID` INT AUTO_INCREMENT PRIMARY KEY,
   `Nombre` VARCHAR(255)
 );
-
-CREATE TABLE `Tag` (
-  `ID` INT AUTO_INCREMENT PRIMARY KEY,
-  `Nombre` VARCHAR(255)
-);
-
-CREATE TABLE `Usuario` (
-  `ID` INT AUTO_INCREMENT PRIMARY KEY,
-  `Nombre` VARCHAR(255),
-  `Contraseña` VARCHAR(255),
-  `Correo` VARCHAR(255),
-  `Codigo_Administrador` VARCHAR(255)
-);
-
-CREATE TABLE `Compra` (
-  `ID` INT AUTO_INCREMENT PRIMARY KEY,
-  `Fecha` DATE,
-  `Cliente_ID` INT,
-  `Cantidad` INT,
-  `Usuario_ID` INT,
-  `Lote_ID` INT,
-  FOREIGN KEY (`Cliente_ID`) REFERENCES `Cliente` (`ID`),
-  FOREIGN KEY (`Usuario_ID`) REFERENCES `Usuario` (`ID`),
-  FOREIGN KEY (`Lote_ID`) REFERENCES `Lote` (`ID`)
-);
-
-CREATE TABLE `Categoriza` (
-  `Bodega_ID` INT,
-  `Tag_ID` INT,
-  UNIQUE (`Bodega_ID`, `Tag_ID`),
-  FOREIGN KEY (`Bodega_ID`) REFERENCES `Bodega` (`ID`),
-  FOREIGN KEY (`Tag_ID`) REFERENCES `Tag` (`ID`)
-);
-
-CREATE TABLE `Almacena` (
-  `ID` INT AUTO_INCREMENT PRIMARY KEY,
-  `Bodega_ID` INT,
-  `Lote_ID` INT,
-  FOREIGN KEY (`Bodega_ID`) REFERENCES `Bodega` (`ID`),
-  FOREIGN KEY (`Lote_ID`) REFERENCES `Lote` (`ID`)
-);
-
-CREATE TABLE `Administra` (
-  `Usuario_ID` INT,
-  `Bodega_ID` INT,
-  `Tipo` VARCHAR(255),
-  UNIQUE (`Usuario_ID`, `Bodega_ID`),
-  FOREIGN KEY (`Usuario_ID`) REFERENCES `Usuario` (`ID`),
-  FOREIGN KEY (`Bodega_ID`) REFERENCES `Bodega` (`ID`)
-);
-
--- Create indexes
-CREATE UNIQUE INDEX `Categoriza_index_0` ON `Categoriza` (`Bodega_ID`, `Tag_ID`);
-CREATE UNIQUE INDEX `Administra_index_1` ON `Administra` (`Usuario_ID`, `Bodega_ID`);
--- main.js
+++ main.js
@@ -14,9 +14,9 @@ app.use(express.json());
 // ------------- Conexion a la base de datos -------------
 const pool = mysql.createPool({
   host: 'db',
-  user: 'alfajor',
-  password: 'alfajor',
-  database: 'inventario',
+  user: 'Backend',
+  password: 'Backend',
+  database: 'Ejemplo',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -24,13 +24,13 @@ const pool = mysql.createPool({
 
 // ------------- Inicial -------------
 app.get("/", (req, res) => {
-  res.send(`<h1>¡Hola, este es el backend de Inventario Facilito!</h1>
+  res.send(`<h1>¡Hola, este es un ejemplo de como puedes crear la documentacion!</h1>
   <p> Aquí están las rutas separadas por vistas </p>
   
   <!-- Vista login -->
   
   <input type="checkbox" id="collapse-toggle1">
-  <label for="collapse-toggle1" class="collapse-btn boton"> Vista login</label>
+  <label for="collapse-toggle1" class="collapse-btn boton"> Vista ejemplo</label>
   <div id="collapse-content1" class="collapse-content">
     
     <!-- Metodos de la vista -->
@@ -52,84 +52,6 @@ app.get("/", (req, res) => {
   </div>
   <br/>
   
-  <!-- Vista Crear usuarios -->
-  
-  <input type="checkbox" id="collapse-toggle3">
-  <label for="collapse-toggle3" class="collapse-btn boton"> Vista Crear usuarios</label>
-  <div id="collapse-content3" class="collapse-content">
-    
-    <!-- Metodos de la vista -->
-  
-    <input type="checkbox" id="collapse-toggle4">
-    <label for="collapse-toggle4" class="collapse-btn botonpost">/crear_usuario {metodo: post}</label>
-    <div id="collapse-content4" class="collapse-content">
-      <h3>Ruta</h3>
-      <p>/crear_usuario</p>
-      <p>Metodo: POST</p>
-      <h3>Descripcion</h3>
-      <p>Ruta para crear un usuario</p>
-      <h3>Parametros</h3>
-      <p>{ Nombre: "String", Correo: "String", Contraseña: "String"}</p>
-      <h3>Respuesta</h3>
-      <p> {error 500}(error al crear usuario): Error al crear usuario</p>
-      <p> {respuesta ok}: Usuario creado exitosamente </p>
-    </div>
-  </div>
-  <br/>
-
-  <!-- Vista bodegas -->
-
-  <input type="checkbox" id="collapse-toggle5">
-  <label for="collapse-toggle5" class="collapse-btn boton"> Vista bodegas</label>
-  <div id="collapse-content5" class="collapse-content">
-
-    <!-- Metodos de la vista -->
-
-    <input type="checkbox" id="collapse-toggle6">
-    <label for="collapse-toggle6" class="collapse-btn botonget">/obtener_bodegas {metodo: get}</label>
-    <div id="collapse-content6" class="collapse-content">
-
-      <h3>Ruta</h3>
-      <p>/obtener_bodegas</p>
-      <p>Metodo: GET</p>
-      <h3>Descripcion</h3>
-      <p>Ruta para obtener las bodegas de un usuario</p>
-      <h3>Parametros</h3>
-      <p>{ Usuario_ID: "Number"}</p>
-      <h3>Respuesta</h3>
-      <p> {error 500}(error al obtener datos): Error al obtener datos</p>
-      <p> {respuesta ok}:{"ID": "Number","Nombre": "String"} </p>
-    </div>
-  </div>
-  <br/>
-
-  <!-- Vista Crear Bodegas -->
-
-  <input type="checkbox" id="collapse-toggle7">
-  <label for="collapse-toggle7" class="collapse-btn boton"> Vista Crear Bodegas</label>
-
-  <div id="collapse-content7" class="collapse-content">
-
-    <!-- Metodos de la vista -->
-
-    <input type="checkbox" id="collapse-toggle8">
-    <label for="collapse-toggle8" class="collapse-btn botonpost">/crear_bodega {metodo: post}</label>
-
-    <div id="collapse-content8" class="collapse-content">
-
-      <h3>Ruta</h3>
-      <p>/crear_bodega</p>
-      <p>Metodo: POST</p>
-      <h3>Descripcion</h3>
-      <p>Ruta para crear una bodega</p>
-      <h3>Parametros</h3>
-      <p>{ Nombre: "String", Usuario_ID: "Number"}</p>
-      <h3>Respuesta</h3>
-      <p> {error 500}(error al crear bodega): Error al crear bodega</p>
-      <p> {respuesta ok}: Bodega creada exitosamente </p>
-    </div>
-  </div>
-  <br/>
   
   <style>
     .boton {
@@ -185,51 +107,25 @@ app.get("/", (req, res) => {
 
 // ------------ Rutas por vistas ------------
 
-//    --------- login  ----------------------
-
-app.post('/login', (req, res) => {
-  const {Correo, Contraseña } = req.body;
-  // Usar ? como marcador de posición para los valores de la consulta
-  const sql = 'SELECT Nombre, Contraseña FROM Usuario WHERE Correo = ? and Contraseña = ?';
-  console.log(req.body);
-  
-  pool.query(sql, [Correo,Contraseña], (error, results) => {
-    if (error) {
-      console.error('Error al ejecutar la consulta:', error.stack);
-      res.status(500).send('Error al obtener datos');
-      return;
-    }
-
-    if (results.length === 0) {
-      res.status(401).send('Usuario o contraseña incorrectos');
-      return;
-    }
-
-    res.json(results);
-  });
-});
-
-//    --------- Crear usuario ---------------
-
-app.post('/crear_usuario', (req, res) => {
-  const { Nombre, Correo, Contraseña } = req.body;
-  const sql = 'INSERT INTO Usuario (Nombre, Correo, Contraseña) VALUES (?, ?, ?)';
-  pool.query(sql, [Nombre, Correo, Contraseña], (error, results) => {
+app.post('/ejemplo', (req, res) => {
+  const { Nombre } = req.body;
+  const sql = 'INSERT INTO Ejemplo (Nombre) VALUES (?)';
+  pool.query(sql, [Nombre], (error, results) => {
     if (error) {
       console.error('Error al ejecutar la consulta:', error.stack);
       res.status(500).send('Error al crear usuario');
       return;
     }
-    res.status(201).send('Usuario creado exitosamente');
+    res.status(201).send('Ejemplo creado exitosamente');
   });
 });
 
-//    --------- Vista bodegas ------------
 
-app.get('/obtener_bodegas', (req, res) => {
-  const { Usuario_ID } = req.body;
-  const sql = 'SELECT B.*  FROM Bodega B JOIN Administra A ON B.ID = A.Bodega_ID WHERE A.Usuario_ID = ?; ';
-  pool.query(sql,[Usuario_ID], (error, results) => {
+app.get('/Obtener_Ejemplo/:Usuario_ID', (req, res) => {
+  const { Usuario_ID } = req.params;
+  
+  const sql = 'SELECT * FROM Ejemplo WHERE Nombre = ?';
+  pool.query(sql, [Usuario_ID], (error, results) => {
     if (error) {
       console.error('Error al ejecutar la consulta:', error.stack);
       res.status(500).send('Error al obtener datos');
@@ -237,40 +133,7 @@ app.get('/obtener_bodegas', (req, res) => {
     }
     res.json(results);
   });
-  });
-
-  // -------------- Vista Crear Bodegas --------------
-
-  app.post('/crear_bodega', (req, res) => {
-    const { Nombre, Usuario_ID } = req.body;
-    
-    const sql = 'INSERT INTO Bodega (Nombre) VALUES (?);';
-    pool.query(sql, [Nombre], (error, results) => {
-      
-      if (error) {
-        console.error('Error al ejecutar la consulta:', error.stack);
-        res.status(500).send('Error al crear bodega');
-        return;
-      }
-
-      const bodegaId = results.insertId;
-      const sqlInsertAdministra = 'INSERT INTO Administra (Usuario_ID, Bodega_ID, Tipo) VALUES (?, ?, ?);';
-      console.log(bodegaId);  
-      pool.query(sqlInsertAdministra, [Usuario_ID, bodegaId, 'Propietario'], (error, results) => {
-        if (error) {
-          console.error('Error al ejecutar la consulta:', error.stack);
-          res.status(500).send('Error al crear bodega');
-          return;
-        }
-      });
-
-      res.status(201).send('Bodega creada exitosamente');
-      
-    
-    });
-  });
-
-
+});
 
 
 // ------------- Print del puerto -------------
